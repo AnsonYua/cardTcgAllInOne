e57a05db243b9cf489a76b1baf413d7247fc11c0
awsanson <awsanson@gmail.com>
2025-07-20 19:31:55 +0800

    no message

--- cardBackend/kill.sh
+++ cardBackend/kill.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Find the process using port 3000
-PID=$(lsof -ti :3000)
+# Find the process using port 8080
+PID=$(lsof -ti :8080)
 
 # Check if a process was found
 if [ -n "$PID" ]; then
-  echo "Killing process $PID using port 3000."
+  echo "Killing process $PID using port 8080."
   kill -9 $PID
 else
-  echo "No process is using port 3000."
+  echo "No process is using port 8080."
 fi
